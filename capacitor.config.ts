66d9d7300df005869085988a42a8fdf0c5754127
jesus <dc17065@gmail.com>
2024-05-02 21:04:37 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Jesus-grupoE191-examen2blank',
+  webDir: 'www'
+};
+
+export default config;
